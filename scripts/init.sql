c3248399f8aa1c552b30f253018baaf6575a863e
hadil010 <130673320+hadil010@users.noreply.github.com>
2025-11-10 21:05:45 -0700

    Update init.sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,1 +1,51 @@
+/*
+-- =============================================================
+-- Create DataWarehouse and Schemas
+-- =============================================================
+-- Overview:
+--     This script builds a new database called 'DataWarehouse'.
+--     If it already exists, it will be deleted and created again.
+--     After that, three schemas will be set up:
+--     bronze, silver, and gold.
+--
+-- Note:
+--     Remove the comment marks below only if you have a backup.
+--     Running this will delete everything inside the current
+--     'DataWarehouse' database. Proceed carefully.
+-- =============================================================
+
+*/
+
+
+USE MASTER;
+GO
+
+--Drop and recreate the Datawarehouse database
+
+IF EXISTS (SELECT 1
+FROM sys.databases
+WHERE name = 'Datawarehouse')
+BEGIN
+DROP DATABASE
+Datawarehouse
+END
+GO 
+
+
+-- Create the 'Datawarehouse' database
+CREATE DATABASE Datawarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
 
