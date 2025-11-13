7d314a911151710eca3c57652d47b2a577f70477
hadil010 <130673320+hadil010@users.noreply.github.com>
2025-11-13 09:41:34 -0700

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -21,7 +21,8 @@ cst_firstname NVARCHAR(50),
 cst_lastname NVARCHAR(50),
 cst_material_status NVARCHAR(50),
 cst_gndr NVARCHAR(50),
-cst_create_date DATE
+cst_create_date DATE,
+dwh_create_date DATETIME2 DEFAULT GETDATE()
 ) ;
 GO 
 
@@ -36,7 +37,8 @@ CREATE TABLE silver.crm_prd_info (
     prd_cost     INT,
     prd_line     NVARCHAR(50),
     prd_start_dt DATETIME,
-    prd_end_dt   DATETIME
+    prd_end_dt   DATETIME,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 ); 
 GO 
 
@@ -54,7 +56,8 @@ CREATE TABLE silver.crm_sales_details (
     sls_due_dt   INT,
     sls_sales    INT,
     sls_quantity INT,
-    sls_price    INT
+    sls_price    INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -64,7 +67,9 @@ GO
 
 CREATE TABLE silver.erp_loc_a101 (
     cid    NVARCHAR(50),
-    cntry  NVARCHAR(50)
+    cntry  NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+
 );
 GO
 
@@ -75,7 +80,8 @@ GO
 CREATE TABLE silver.erp_cust_az12 (
     cid    NVARCHAR(50),
     bdate  DATE,
-    gen    NVARCHAR(50)
+    gen    NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -87,6 +93,7 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     id           NVARCHAR(50),
     cat          NVARCHAR(50),
     subcat       NVARCHAR(50),
-    maintenance  NVARCHAR(50)
+    maintenance  NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO 
